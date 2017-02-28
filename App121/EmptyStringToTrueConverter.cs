54ba793cf8f864af78f1912b47162c77dc3654ea
sonnemaf <fons.sonnemans@reflectionit.nl>
2017-02-28 12:00:55 +0100

    Add project files.

---
+++ App121/EmptyStringToTrueConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace App121 {
+public class EmptyStringToTrueConverter : IValueConverter {
+    public bool IsInverted { get; set; }
+
+    public object Convert(object value, Type targetType, object parameter, string language) {
+        var txt = (string)value;
+        return string.IsNullOrEmpty(txt) ^ IsInverted;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) {
+        return DependencyProperty.UnsetValue;
+    }
+}
+}
